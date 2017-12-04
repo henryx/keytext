08d355e7b7364b0e80de5c644786862b0bdd089d
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-12-04 22:51:23 +0100

    Started project

---
+++ keytext.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Enrico Bianchi"
+__credits__ = ["Enrico Bianchi", ]
+__license__ = "MIT"
+__maintainer__ = "Enrico Bianchi"
+__version__ = "1.0.0"
+
+
+def main():
+    """
+    Main function
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
