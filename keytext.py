2d7dde4a5ed4149ea135a0a765950e1b79dc2763
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-12-04 23:41:06 +0100

    Added argument parser

--- keytext.py
+++ keytext.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import argparse
 import collections
+import sys
 
 __author__ = "Enrico Bianchi"
 __credits__ = ["Enrico Bianchi", ]
 __license__ = "MIT"
 __maintainer__ = "Enrico Bianchi"
 __version__ = "1.0.0"
+
+
+def init_args():
+    """
+    Argument parser initialization
+    :return: An object that parse arguments passed from command line
+    """
+
+    args = argparse.ArgumentParser(description="keytext")
+    args.add_argument("filename", help="File to parse")
+
+    return args
 
 
 def main():
@@ -25,6 +39,8 @@ def main():
         "dx": fingers.copy(),
         "sx": fingers.copy(),
     }
+
+    args = init_args().parse_args(sys.argv[1:])
 
 
 if __name__ == '__main__':
